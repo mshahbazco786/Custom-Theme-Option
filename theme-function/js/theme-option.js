62a71566469206a736f7d7bd810c016334e7c3d2
Muhammad Shahbaz <72975073+mshahbazco786@users.noreply.github.com>
2023-01-27 09:55:01 +0500

    Update theme-option.js

--- theme-function/js/theme-option.js
+++ theme-function/js/theme-option.js
@@ -29,48 +29,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
         // console.log(authorImagePreview.src);
     });
 
-});
-
-
-
-
-// Create Custom Event Using on() function
-
-
-
-// (function( $ ) {
-// 	'use strict';
-
-// 	$(function() {
-		
-// 		$('#upload_image').click(open_custom_media_window);
-
-// 		function open_custom_media_window() {
-// 			if (this.window === undefined) {
-// 				this.window = wp.media({
-// 					title: 'Insert Image',
-// 					library: {type: 'image'},
-// 					multiple: false,
-// 					button: {text: 'Insert Image'}
-// 				});
-
-// 				var self = this;
-// 				this.window.on('select', function() {
-// 					var response = self.window.state().get('selection').first().toJSON();
-//                     console.log(response);
-// 					$('.wp_attachment_id').val(response.id);
-// 					$('.image').attr('src', response.url);
-//                      $('.image').show();
-// 				});
-// 			}
-
-// 			this.window.open();
-// 			return false;
-// 		}
-// 	});
-// })( jQuery );
-
-document.addEventListener('DOMContentLoaded', (event) => {
+    // CREATE SIDE BAR TABS FOR THEME OPTIONS
     const tabs = document.querySelector('.tabs');
     const tabButtons = tabs.querySelectorAll('[role="tab"]');
     const tabPanels = tabs.querySelectorAll('[role="tabpanel"]');   
@@ -97,22 +56,3 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     }     
 });
-
-
-jQuery(document).ready(function($) {
-    $('#input#upload_image_button').click(function() {
-        console.log('heloo');
-        tb_show('Upload a logo', 'media-upload.php?referer=wptuts-settings&type=image&TB_iframe=true&post_id=0', false);
-        return false;
-    });
-});
-
-window.send_to_editor = function(html) {
-    var image_url = $('img',html).attr('src');
-    $('#logo_url').val(image_url);
-    tb_remove();
-    $('#upload_logo_preview img').attr('src',image_url);
-	$('#submit_options_form').trigger('click');
-}
-
-
